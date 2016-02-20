ca9242af350bbe086324e2fb0831ca7958847709
Alec Garcia <AlecAnthonyGarcia@gmail.com>
2016-02-19 19:13:15 -0800

    Created GCE Module to fetch joke from server

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,14 +1,22 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.calialec.builditbigger.Joker;
+import com.calialec.builditbigger.backend.myApi.MyApi;
 import com.calialec.builditbigger.jokeshowcase.JokeShowcaseActivity;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -43,9 +51,43 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view) {
-        Intent intent = new Intent(this, JokeShowcaseActivity.class);
-        intent.putExtra("joke", new Joker().getJoke());
-        startActivity(intent);
+        new JokeEndpointsAsyncTask().execute(this);
     }
 
+}
+
+class JokeEndpointsAsyncTask extends AsyncTask<Context, Void, String> {
+    private static MyApi myApiService = null;
+    private Context context;
+
+    @Override
+    protected String doInBackground(Context... params) {
+        if(myApiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    .setRootUrl("https://udacity-build-it-bigger-1227.appspot.com/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            myApiService = builder.build();
+        }
+
+        context = params[0];
+
+        try {
+            return myApiService.tellJoke().execute().getData();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Intent intent = new Intent(context, JokeShowcaseActivity.class);
+        intent.putExtra("joke", result);
+        context.startActivity(intent);
+    }
 }
